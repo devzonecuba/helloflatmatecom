571eb9e82c3e71cc2fafec195718e7461e6296d2
yffrasquero <yffrasquero@finlay.edu.cu>
2019-04-18 14:42:53 -0400

    HelloOwner Done

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -10,9 +10,19 @@ class QuestionController extends AbstractController
     /**
      * @Route("/hellorooms", name="hellorooms")
      */
-    public function index()
+    public function hellorooms()
     {
         return $this->render('question/hellorooms.html.twig', [
+            'controller_name' => 'QuestionController',
+        ]);
+    }
+
+    /**
+     * @Route("/helloowners", name="helloowners")
+     */
+    public function helloowners()
+    {
+        return $this->render('question/helloowners.html.twig', [
             'controller_name' => 'QuestionController',
         ]);
     }
